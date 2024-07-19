7fa33273bf35a05890b8bbd6d7a25cc744e7c662
TanujaKhot <ttkhot2004@gmail.com>
2024-07-19 19:53:31 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,17 @@
+function Solve(val) {
+   var a = document.getElementById('res');
+   a.value += val;
+}
+function Result() {
+   var num1 = document.getElementById('res').value;
+   var num2 = eval(num1);
+   document.getElementById('res').value = num2;
+}
+function Clear() {
+   var inp = document.getElementById('res');
+   inp.value = '';
+}
+function Back() {
+   var ev = document.getElementById('res');
+   ev.value = ev.value.slice(0,-1);
+}
